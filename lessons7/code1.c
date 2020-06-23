77bdbab2ac387bda0c4813a61c1c4cbe6af149cf
wfm666 <810760487@qq.com>
2020-06-23 16:48:44 +0800

    第七次作业

---
+++ lessons7/code1.c
@@ -0,0 +1,40 @@
+int i=0;
+int j=0;
+int k=0;
+ 
+void setup()
+{
+  for(i=2;i<=9;i++)
+  {
+    pinMode(i,OUTPUT);
+  }
+  for(i=2;i<=9;i++)
+  {
+    digitalWrite(i,HIGH);
+  }
+}
+ 
+void loop()
+{
+  int num[10][7]={
+    {2,3,4,5,6,7},
+    {3,4},
+    {2,3,5,6,8},
+    {2,3,4,5,8},
+    {3,4,7,8},
+    {2,4,5,7,8},
+    {5,6,7,8},
+    {2,3,4},
+    {2,3,4,5,6,7,8},
+    {2,3,4,7,8}
+  };
+  for(i=0;i<10;i++)
+  {
+    for(j=0;j<7;j++)
+    {
+      digitalWrite(num[i][j],LOW); 
+      digitalWrite(num[i][j],HIGH);
+      delay(1000);
+    }
+  }
+}
